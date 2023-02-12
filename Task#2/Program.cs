7ab5ab3dd45b790e3bd50d479a9dae06f63cb8c0
Ramses <rexid44@gmail.com>
2023-02-12 23:37:34 +1000

    Task#2

--- Task#2/Program.cs
+++ Task#2/Program.cs
@@ -6,4 +6,37 @@ Console.WriteLine("Input the second number");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input the third number");
 int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+
+if (b > max)
+{
+    max = b;
+    if (c > max)
+    {
+        max = c;
+        Console.Write("The greatest number - ");
+        Console.Write(max);
+    }
+    else
+    {
+        Console.Write("The greatest number - ");
+        Console.Write(max);
+    }
+}
+else 
+{
+    if (c > max)
+    {
+        max = c;
+        Console.Write("The greatest number - ");
+        Console.Write(max);
+    }
+    else
+    {
+        Console.Write("The greatest number - ");
+        Console.Write(max);
+    } 
+}
+
+//Возможно более оптимальное решение через case
 
