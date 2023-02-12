054ec2186103f6e364789df18254a726d0b8333c
Ramses <rexid44@gmail.com>
2023-02-12 23:46:48 +1000

    Task#3

--- Task#3/Program.cs
+++ Task#3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Gimme a number");
+int EVEN = Convert.ToInt32(Console.ReadLine());
+
+if (EVEN % 2 == 0)
+{
+    Console.WriteLine("Yes, the number is even");
+}
+else
+{
+    Console.WriteLine("No, the number isn't even");
+}
