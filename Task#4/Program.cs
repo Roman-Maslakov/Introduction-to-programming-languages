26f7a29d616b855a74bc43a1fbf2787e2e63db4f
Ramses <rexid44@gmail.com>
2023-02-13 00:06:22 +1000

    Task#4

--- Task#4/Program.cs
+++ Task#4/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Gimme a number");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Here is your row of even numbers");
+for (int i = 1; i <= N; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i);
+        Console.Write(", ");
+    }
+}
